de5a58d14bc7dd2507acf9c0ee25ca9ff4762a9c
whitepanther <pansuriya.kishan21@gmail.com>
2021-11-16 14:21:18 +0530

    Update tabLeaveAlert.js

--- lwc/tabLeaveAlert/tabLeaveAlert.js
+++ lwc/tabLeaveAlert/tabLeaveAlert.js
@@ -1,6 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class TabLeaveAlert extends LightningElement {
+
+    @track confirmBoxOpened = false;
+    @track lastClickTimeStamp;
 
     handleInput(event) {
         if (event.target.name == 'input1') {
@@ -16,11 +19,33 @@ export default class TabLeaveAlert extends LightningElement {
         
         //todo: remove below event listener on submit button or proper action.
         //so that same functionality don't execute at un-wanted event
+                
         window.addEventListener('beforeunload', (event) => {
-            //prevents page reload.         
+            //prevents page reload.    
+            console.log('beforeunload event.');     
             event.preventDefault();
             event.returnValue = 'sample value';
         });
+
+        //todo: remove below event listener on submit button or proper action.        
+        document.addEventListener("click", (e) =>{
+            console.log(e);
+            let currentClickTimeStamp = e.timeStamp;
+            //check if click is outside compo
+            if(e.target.tagName === 'BODY' && 
+                !this.confirmBoxOpened && 
+                this.lastClickTimeStamp !== currentClickTimeStamp){
+
+                this.lastClickTimeStamp = e.timeStamp;
+                this.confirmBoxOpened = true;
+                let ans = confirm('leave form filling?');
+                if(!ans) {
+                    this.confirmBoxOpened = false;
+                    e.stopPropagation();
+                    e.preventDefault();                  
+                }                
+            }
+    	}, true);
     }
 
     handleButton1() {
