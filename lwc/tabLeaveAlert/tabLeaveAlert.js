2c158b3a5995ceb940033db145c52b0623bf44c6
whitepanther <pansuriya.kishan21@gmail.com>
2021-11-17 10:58:51 +0530

    Remove event listener fix.
    function reference issue on remove event listener fix.

--- lwc/tabLeaveAlert/tabLeaveAlert.js
+++ lwc/tabLeaveAlert/tabLeaveAlert.js
@@ -4,6 +4,40 @@ export default class TabLeaveAlert extends LightningElement {
 
     @track confirmBoxOpened = false;
     @track lastClickTimeStamp;
+    
+    preventReload = (e) => {
+        //prevents page reload.    
+        console.log('beforeunload prevented.');     
+        e.preventDefault();
+        e.returnValue = 'sample value';
+    }
+
+    preventTabSwitch = (e) => {
+        console.log('click detected..');
+        let currentClickTimeStamp = e.timeStamp;
+        //check if click is outside compo
+        if(e.target.tagName === 'BODY' && 
+            !this.confirmBoxOpened && 
+            this.lastClickTimeStamp !== currentClickTimeStamp){
+
+            this.lastClickTimeStamp = e.timeStamp;
+            this.confirmBoxOpened = true;
+            let ans = confirm('leave form filling?');
+            if(!ans) {
+                //if warning respected stay on the form
+                this.confirmBoxOpened = false;
+                e.stopPropagation();
+                e.preventDefault();                  
+            }else{
+                //remove listeners as disconnected callback won't be executed
+                //when main tabs are switched
+                console.log('removing registered events as user is willing to leave form.');
+                console.log(this);            
+                window.removeEventListener('beforeunload', this.preventReload, true);
+                document.removeEventListener('click', this.preventTabSwitch, true);
+            }
+        }
+    }
 
     handleInput(event) {
         if (event.target.name == 'input1') {
@@ -16,40 +50,17 @@ export default class TabLeaveAlert extends LightningElement {
             console.log('handle input3.');
         }
         this.dispatchEvent(new CustomEvent('childformmodified'));
-        
-        //todo: remove below event listener on submit button or proper action.
-        //so that same functionality don't execute at un-wanted event
-                
-        window.addEventListener('beforeunload', (event) => {
-            //prevents page reload.    
-            console.log('beforeunload event.');     
-            event.preventDefault();
-            event.returnValue = 'sample value';
-        });
-
-        //todo: remove below event listener on submit button or proper action.        
-        document.addEventListener("click", (e) =>{
-            console.log(e);
-            let currentClickTimeStamp = e.timeStamp;
-            //check if click is outside compo
-            if(e.target.tagName === 'BODY' && 
-                !this.confirmBoxOpened && 
-                this.lastClickTimeStamp !== currentClickTimeStamp){
-
-                this.lastClickTimeStamp = e.timeStamp;
-                this.confirmBoxOpened = true;
-                let ans = confirm('leave form filling?');
-                if(!ans) {
-                    this.confirmBoxOpened = false;
-                    e.stopPropagation();
-                    e.preventDefault();                  
-                }                
-            }
-    	}, true);
+        window.addEventListener('beforeunload', this.preventReload, true);            
+        document.addEventListener('click', this.preventTabSwitch, true);
     }
 
     handleButton1() {
         console.log('handle button1.');
+    }
+
+    disconnectedCallback(){
+        window.removeEventListener('beforeunload', this.preventReload, true);
+        document.removeEventListener('click', this.preventTabSwitch, true);
     }
 
 }
